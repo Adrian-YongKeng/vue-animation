503f8b134a1d5dd073c0b2036fc0a5ca402f7656
Adrian <yy_1123@hotmail.com>
2025-06-01 16:23:45 +0800

    adding css keyframes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -34,18 +34,19 @@ export default {
 
 <style>
 /*enter class*/
-.toast-enter-from {
-  opacity: 0;
-  transform: translateY(-60px);
-}
+/* .toast-enter-from { */
+/* opacity: 0; */
+/* transform: translateY(-60px); */
+/* } */
 /* enter-to default state so no need code out  */
-.toast-enter-to {
-  opacity: 1;
-  transform: translateY(0);
-}
+/* .toast-enter-to { */
+/* opacity: 1; */
+/* transform: translateY(0); */
+/* } */
 
 .toast-enter-active {
-  transition: all 0.3s ease;
+  /* transition: all 0.3s ease; */
+  animation: wobble 0.5s ease;
 }
 /*leave class*/
 /* leave-from default state so no need code out  */
@@ -53,14 +54,38 @@ export default {
   opacity: 1;
   transform: translateY(0);
 }
-
 .toast-leave-to {
   opacity: 0;
   transform: translateY(-60px);
 }
-
 .toast-leave-active {
   transition: all 0.3s ease;
+}
+
+@keyframes wobble {
+  0% {
+    transform: translateY(-60px);
+    opacity: 0;
+  }
+  50% {
+    transform: translateY(0);
+    opacity: 1;
+  }
+  60% {
+    transform: translateX(8px);
+  }
+  70% {
+    transform: translateX(-8px);
+  }
+  80% {
+    transform: translateX(4px);
+  }
+  90% {
+    transform: translateX(-4px);
+  }
+  100% {
+    transform: translateX(0);
+  }
 }
 
 /* .fade-enter-from {
